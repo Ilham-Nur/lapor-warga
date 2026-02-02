82adf7a30aa27df7c300952f4d2dbd031c5147d9
Ilham-Nur <ilonoer26@gmail.com>
2026-02-02 15:15:25 +0700

    push to test

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
-
 return [
     'proxies' => '*',
-    'headers' => Request::HEADER_X_FORWARDED_ALL,
+
+    /*
+     |------------------------------------------------------------------
+     | Trusted Proxy Headers
+     |------------------------------------------------------------------
+     | Laravel 12 menggunakan bitmask integer, bukan constant lama.
+     */
+    'headers' =>
+        1   | // HEADER_FORWARDED
+        2   | // HEADER_X_FORWARDED_FOR
+        4   | // HEADER_X_FORWARDED_HOST
+        8   | // HEADER_X_FORWARDED_PROTO
+        16,   // HEADER_X_FORWARDED_PORT
 ];
